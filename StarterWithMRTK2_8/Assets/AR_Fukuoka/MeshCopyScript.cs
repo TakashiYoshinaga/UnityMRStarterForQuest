a409d5cda092c6be6244c1bff2a6e625bc346e70
dengxian-xreal <134575521+dengxian-xreal@users.noreply.github.com>
2024-03-14 16:19:11 +0800

    Save mesh as obj

--- StarterWithMRTK2_8/Assets/AR_Fukuoka/MeshCopyScript.cs
+++ StarterWithMRTK2_8/Assets/AR_Fukuoka/MeshCopyScript.cs
@@ -8,6 +8,7 @@ using System.Collections.Generic;
 using Microsoft.MixedReality.Toolkit.UI;
 using TMPro;
 using UnityEngine;
+using System.IO;
 
 public class MeshCopyScript : MonoBehaviour
 {
@@ -36,11 +37,47 @@ public class MeshCopyScript : MonoBehaviour
                     meshObject.transform.rotation=room.transform.rotation;
                     meshObject.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
                     _Button.gameObject.SetActive(false);
+
+                    // Save the mesh to a file
+                    SaveMeshAsOBJ(_MeshFilter.mesh, "ExportedMesh");
                 }
             } 
         });
     }
 
+    void SaveMeshAsOBJ(Mesh mesh, string filenameWithoutExtension)
+    {
+        string path = Path.Combine(Application.persistentDataPath, filenameWithoutExtension + ".obj");
+        StreamWriter writer = new StreamWriter(path);
+
+        writer.WriteLine("# Exported Mesh");
+        foreach (Vector3 vertex in mesh.vertices)
+        {
+            writer.WriteLine("v " + vertex.x + " " + vertex.y + " " + vertex.z);
+        }
+
+        writer.WriteLine("# " + mesh.vertices.Length + " vertices");
+
+        foreach (Vector3 normal in mesh.normals)
+        {
+            writer.WriteLine("vn " + normal.x + " " + normal.y + " " + normal.z);
+        }
+
+        writer.WriteLine("# " + mesh.normals.Length + " normals");
+
+        for (int i = 0; i < mesh.subMeshCount; i++)
+        {
+            int[] triangles = mesh.GetTriangles(i);
+            for (int j = 0; j < triangles.Length; j += 3)
+            {
+                writer.WriteLine("f " + (triangles[j] + 1) + " " + (triangles[j + 1] + 1) + " " + (triangles[j + 2] + 1));
+            }
+        }
+
+        writer.Close();
+
+        Debug.Log("Mesh saved as OBJ to " + path);
+    }
     // Update is called once per frame
     void Update()
     {
